d15e0856607c14ffcbf644cb79382c4f67000a6a
Gorthdzx <krislaksana123@gmail.com>
2024-11-20 09:43:00 +0700

    delete : unused import

--- app/src/main/java/app/culturedev/cultureconnect/ui/splash/onboarding/FirstScreenFragment.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/splash/onboarding/FirstScreenFragment.kt
@@ -1,13 +1,10 @@
 package app.culturedev.cultureconnect.ui.splash.onboarding
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import app.culturedev.cultureconnect.R
 import app.culturedev.cultureconnect.databinding.FragmentFirstScreenBinding
