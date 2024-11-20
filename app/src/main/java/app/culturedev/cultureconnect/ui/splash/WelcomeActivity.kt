e8528e348a5130d56cfcd5d55bd9371b1d7a0ac0
Gorthdzx <krislaksana123@gmail.com>
2024-11-20 09:27:19 +0700

    refactor : renamed to welcome  activity

--- app/src/main/java/app/culturedev/cultureconnect/ui/splash/WelcomeActivity.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/splash/WelcomeActivity.kt
@@ -7,15 +7,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import app.culturedev.cultureconnect.R
 
-class SplashActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar?.hide()
     }
 }
