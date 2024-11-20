407e962e804432deda1ef3784a8776caae54b946
Gorthdzx <krislaksana123@gmail.com>
2024-11-20 09:42:25 +0700

    style : addded first screen onboarding

---
+++ app/src/main/java/app/culturedev/cultureconnect/ui/splash/onboarding/FirstScreenFragment.kt
@@ -0,0 +1,32 @@
+package app.culturedev.cultureconnect.ui.splash.onboarding
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import app.culturedev.cultureconnect.R
+import app.culturedev.cultureconnect.databinding.FragmentFirstScreenBinding
+
+class FirstScreenFragment : Fragment() {
+    private lateinit var binding: FragmentFirstScreenBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirstScreenBinding.inflate(inflater, container, false)
+        initViews()
+        return binding.root
+    }
+
+    private fun initViews() {
+        binding.btnNext.setOnClickListener {
+            val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+            viewPager?.currentItem = 1
+        }
+    }
+}
