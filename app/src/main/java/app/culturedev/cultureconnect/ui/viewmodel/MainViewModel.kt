120ad974f6b0ec6dd7c2901102d1500709ee8ac4
Gorthdzx <krislaksana123@gmail.com>
2024-12-04 14:57:08 +0700

    delete : unused libs

--- app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/MainViewModel.kt
@@ -1,13 +1,10 @@
 package app.culturedev.cultureconnect.ui.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import app.culturedev.cultureconnect.data.model.UserModel
 import app.culturedev.cultureconnect.data.repository.CafeRepo
-import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: CafeRepo) : ViewModel() {
     fun getSession(): LiveData<UserModel> {
