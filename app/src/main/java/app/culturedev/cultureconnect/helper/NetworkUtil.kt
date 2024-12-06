3e7d69b85c2e68bb7578479b610134d41a557ca3
Gorthdzx <krislaksana123@gmail.com>
2024-12-06 07:30:06 +0700

    feat : added net check function

---
+++ app/src/main/java/app/culturedev/cultureconnect/helper/NetworkUtil.kt
@@ -0,0 +1,28 @@
+package app.culturedev.cultureconnect.helper
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.widget.Toast
+
+object NetworkUtil {
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            //for check internet over Bluetooth
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
+    }
+
+    fun netToast(context: Context) {
+        Toast.makeText(context, "No Internet Connection", Toast.LENGTH_SHORT).show()
+    }
+}
