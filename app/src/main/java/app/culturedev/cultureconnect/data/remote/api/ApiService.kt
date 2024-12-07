2f5ffa0101af6175868dab8dff5097fecd018124
Gorthdzx <krislaksana123@gmail.com>
2024-12-07 13:59:35 +0700

    feat : added login endpoint

--- app/src/main/java/app/culturedev/cultureconnect/data/remote/api/ApiService.kt
+++ app/src/main/java/app/culturedev/cultureconnect/data/remote/api/ApiService.kt
@@ -1,12 +1,11 @@
 package app.culturedev.cultureconnect.data.remote.api
 
-import app.culturedev.cultureconnect.data.response.DataRes
-import app.culturedev.cultureconnect.data.response.LoginRes
+import app.culturedev.cultureconnect.data.response.LoginRequest
+import app.culturedev.cultureconnect.data.response.LoginResponse
 import app.culturedev.cultureconnect.data.response.RegisterRes
+import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
@@ -18,16 +17,9 @@ interface ApiService {
         @Field("password") password: String
     ): RegisterRes
 
-    @FormUrlEncoded
-    @POST("login")
-    suspend fun login(
-        @Field("email") email: String,
-        @Field("password") password: String
-    ): LoginRes
 
-    @GET("data")
-    suspend fun getStories(
-        @Header("Authorization")
-        token: String
-    ): DataRes
+    @POST("auth/login")
+    suspend fun login(
+        @Body body: LoginRequest
+    ): LoginResponse
 }
