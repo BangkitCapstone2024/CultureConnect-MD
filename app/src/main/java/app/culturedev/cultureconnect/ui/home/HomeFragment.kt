b302a3cb5af871edbc7f9e4e74606a1a52efb39c
Gorthdzx <krislaksana123@gmail.com>
2024-12-08 12:45:17 +0700

    feat : added check if user was loginned

--- app/src/main/java/app/culturedev/cultureconnect/ui/home/HomeFragment.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/home/HomeFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import app.culturedev.cultureconnect.R
 import app.culturedev.cultureconnect.databinding.FragmentHomeBinding
 import app.culturedev.cultureconnect.helper.ColorUtils
 import app.culturedev.cultureconnect.helper.NetworkUtil
+import app.culturedev.cultureconnect.ui.auth.login.LoginActivity
 import app.culturedev.cultureconnect.ui.recomendation.DescribeMoodActivity
+import app.culturedev.cultureconnect.ui.viewmodel.HomeViewModel
+import app.culturedev.cultureconnect.ui.viewmodel.factory.FactoryViewModel
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val vm by viewModels<HomeViewModel> { FactoryViewModel.getInstance(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,11 +47,30 @@ class HomeFragment : Fragment() {
 
         ColorUtils.changeStatusBarColor(requireActivity().window, "#1B3E3B")
         toMoodBased()
+        getUsername()
     }
 
     private fun toMoodBased() {
         binding.btnRecommendation.setOnClickListener {
-            startActivity(Intent(activity, DescribeMoodActivity::class.java))
+            vm.getSession().observe(viewLifecycleOwner) {
+                if (it.sessionId.isEmpty()) {
+                    val intent = Intent(requireContext(), LoginActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    startActivity(intent)
+                } else {
+                    startActivity(Intent(requireContext(), DescribeMoodActivity::class.java))
+                }
+            }
+        }
+    }
+
+    private fun getUsername() {
+        vm.getSession().observe(viewLifecycleOwner) {
+            if (it.sessionId.isEmpty()) {
+                binding.username.text = getString(R.string.username)
+            } else {
+                binding.username.text = it.username
+            }
         }
     }
 }
