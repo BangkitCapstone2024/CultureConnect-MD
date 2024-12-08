763c5641588f7ff7718fe78e2fc1a181fc91c92f
Gorthdzx <krislaksana123@gmail.com>
2024-12-08 11:05:34 +0700

    delete : firebase sign up  method

--- app/src/main/java/app/culturedev/cultureconnect/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/auth/register/RegisterActivity.kt
@@ -1,22 +1,14 @@
 package app.culturedev.cultureconnect.ui.auth.register
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import app.culturedev.cultureconnect.databinding.ActivityRegisterBinding
 import app.culturedev.cultureconnect.helper.ColorUtils
 import app.culturedev.cultureconnect.helper.NetworkUtil
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.auth
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
-    private lateinit var auth: FirebaseAuth
-    private var customToken: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +22,6 @@ class RegisterActivity : AppCompatActivity() {
             NetworkUtil.netToast(this)
         }
 
-        auth = Firebase.auth
     }
 
     private fun navigateUp() {
@@ -39,29 +30,6 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    private fun startSignIn() {
-        customToken?.let {
-            auth.signInWithCustomToken(it)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Log.d(TAG, "signInWithCustomToken:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        Log.w(TAG, "signInWithCustomToken:failure", task.exception)
-                        Toast.makeText(
-                            baseContext,
-                            "Authentication failed.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-                        updateUI(null)
-                    }
-                }
-        }
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-    }
 
     companion object {
         private const val TAG = "CustomAuthActivity"
