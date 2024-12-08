695c01bf9b3530886bc053020d23021dbdc7c417
Gorthdzx <krislaksana123@gmail.com>
2024-12-08 10:11:17 +0700

    feat :  added check session function

--- app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/HomeViewModel.kt
@@ -1,7 +1,13 @@
 package app.culturedev.cultureconnect.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import app.culturedev.cultureconnect.data.model.UserModel
 import app.culturedev.cultureconnect.data.repository.CafeRepo
 
 class HomeViewModel(private val repository:CafeRepo) :ViewModel() {
+    fun getSession():LiveData<UserModel>{
+        return repository.getSession().asLiveData()
+    }
 }
