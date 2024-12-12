74fd6f4b248fb1956be08689be7a21d5b0303fd7
Gorthdzx <krislaksana123@gmail.com>
2024-12-12 13:58:41 +0700

    feat: added more detail to cafe

--- app/src/main/java/app/culturedev/cultureconnect/ui/adapter/RecommenderAdapter.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/adapter/RecommenderAdapter.kt
@@ -1,5 +1,7 @@
 package app.culturedev.cultureconnect.ui.adapter
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -7,6 +9,8 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import app.culturedev.cultureconnect.data.response.recommendation.CafeRecommendation
 import app.culturedev.cultureconnect.databinding.ListItemBinding
+import app.culturedev.cultureconnect.helper.Utils
+import app.culturedev.cultureconnect.ui.detail.DetailActivity
 import com.bumptech.glide.Glide
 
 class RecommenderAdapter :
@@ -35,12 +39,35 @@ class RecommenderAdapter :
             binding.tvItemRating.text = data.rating
             binding.tvItemPrice.text = data.price
 
-//            binding.ivItemImage.setOnClickListener {
-//                val context = binding.root.context
-//                val intent = Intent(context, DetailActivity::class.java)
-//                intent.putExtra(Utils.EXTRA_ID, data.)
-//                context.startActivity(intent)
-//            }
+            binding.ivItemImage.setOnClickListener {
+                val context = binding.root.context
+                val intent = Intent(context, DetailActivity::class.java)
+                intent.putExtra(Utils.EXTRA_IMAGE, data.image)
+                intent.putExtra(Utils.EXTRA_CAFE_NAME, data.title)
+                intent.putExtra(Utils.EXTRA_RATING , data.rating)
+                intent.putExtra(Utils.EXTRA_ADDRESS , data.address)
+                intent.putExtra(Utils.EXTRA_PRICE,data.price)
+                context.startActivity(intent)
+            }
+
+            binding.btnItemDetail.setOnClickListener {
+                val context = binding.root.context
+                val intent = Intent(context, DetailActivity::class.java)
+                intent.putExtra(Utils.EXTRA_IMAGE, data.image)
+                intent.putExtra(Utils.EXTRA_CAFE_NAME, data.title)
+                intent.putExtra(Utils.EXTRA_RATING , data.rating)
+                intent.putExtra(Utils.EXTRA_ADDRESS , data.address)
+                intent.putExtra(Utils.EXTRA_PRICE,data.price)
+                context.startActivity(intent)
+            }
+
+            binding.btnItemDirection.setOnClickListener {
+                val context = binding.root.context
+                val intentUri = Uri.parse(data.pageURL)
+                val mapIntent = Intent(Intent.ACTION_VIEW , intentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                context.startActivity(mapIntent)
+            }
         }
     }
 
