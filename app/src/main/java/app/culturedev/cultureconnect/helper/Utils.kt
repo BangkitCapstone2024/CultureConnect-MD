64056efa7d884dc579e60cf3e3ccc0c66bb00b85
Gorthdzx <krislaksana123@gmail.com>
2024-12-12 10:06:32 +0700

    feat: key for send item

--- app/src/main/java/app/culturedev/cultureconnect/helper/Utils.kt
+++ app/src/main/java/app/culturedev/cultureconnect/helper/Utils.kt
@@ -5,6 +5,11 @@ import android.widget.ProgressBar
 
 object Utils {
     const val EXTRA_SESSION = "extra_session"
+    const val PREDICT_MOOD = "predict_mood"
+    const val MOOD_QUOTE = "quote"
+    const val RECOMEN_PLACE= "recomen"
+
+    const val LIST_PLACE = "list_place"
     fun showLoading(isLoading: Boolean, progressBar: ProgressBar) {
         progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
