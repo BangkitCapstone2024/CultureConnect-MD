3772a605c4f9e414fa7a327922b689888e3d0c6c
Gorthdzx <krislaksana123@gmail.com>
2024-12-13 10:07:33 +0700

    feat : get result from viewmodel list cafe function

--- app/src/main/java/app/culturedev/cultureconnect/ui/home/HomeFragment.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/home/HomeFragment.kt
@@ -5,22 +5,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import app.culturedev.cultureconnect.R
+import app.culturedev.cultureconnect.data.result.ResultCafe
 import app.culturedev.cultureconnect.databinding.FragmentHomeBinding
 import app.culturedev.cultureconnect.helper.ColorUtils
 import app.culturedev.cultureconnect.helper.NetworkUtil
+import app.culturedev.cultureconnect.ui.adapter.ListCafeAdapter
 import app.culturedev.cultureconnect.ui.auth.login.LoginActivity
 import app.culturedev.cultureconnect.ui.recomendation.DescribeMoodActivity
 import app.culturedev.cultureconnect.ui.viewmodel.HomeViewModel
-import app.culturedev.cultureconnect.ui.viewmodel.factory.FactoryViewModel
+import app.culturedev.cultureconnect.ui.viewmodel.factory.RecommendationFactoryViewModel
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val vm by viewModels<HomeViewModel> { FactoryViewModel.getInstance(requireContext()) }
+    private val vm by viewModels<HomeViewModel> {
+        RecommendationFactoryViewModel.getInstance(
+            requireContext()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,18 +43,12 @@ class HomeFragment : Fragment() {
         if (!NetworkUtil.isOnline(requireContext())) {
             NetworkUtil.netToast(requireContext())
         }
-        binding.rvRecommendation.apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-//            adapter = RecommendationAdapter()
-        }
-
-        binding.rvAllCafe.apply {
-            layoutManager = LinearLayoutManager(context)
-        }
 
         ColorUtils.changeStatusBarColor(requireActivity().window, "#1B3E3B")
         toMoodBased()
         getUsername()
+        getAllCafe()
+        binding.allCafeProgressBar.visibility = View.INVISIBLE
     }
 
     private fun toMoodBased() {
@@ -59,6 +60,31 @@ class HomeFragment : Fragment() {
                 }
             }
             startActivity(Intent(requireContext(), DescribeMoodActivity::class.java))
+        }
+    }
+
+    private fun getAllCafe() {
+        val listCafeAdapter = ListCafeAdapter()
+        vm.getAllCafeData().observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is ResultCafe.Loading -> {
+                    binding.allCafeProgressBar.visibility = View.VISIBLE
+                }
+
+                is ResultCafe.Success -> {
+                    binding.allCafeProgressBar.visibility = View.INVISIBLE
+                    listCafeAdapter.submitList(result.data.cafeData?.take(10))
+                    binding.rvAllCafe.apply {
+                        layoutManager = LinearLayoutManager(context)
+                        adapter = listCafeAdapter
+                    }
+                }
+
+                is ResultCafe.Error -> {
+                    binding.allCafeProgressBar.visibility = View.INVISIBLE
+                    Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
