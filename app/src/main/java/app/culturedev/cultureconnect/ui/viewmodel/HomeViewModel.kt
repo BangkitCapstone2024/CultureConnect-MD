b7c2826babafb61d03d89db0673f305d8d43f2df
Gorthdzx <krislaksana123@gmail.com>
2024-12-13 10:04:48 +0700

    feat : consume from repo to get list of cafe

--- app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/HomeViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import app.culturedev.cultureconnect.data.model.UserModel
-import app.culturedev.cultureconnect.data.repository.CafeRepo
+import app.culturedev.cultureconnect.data.repository.RecommendationRepository
 
-class HomeViewModel(private val repository:CafeRepo) :ViewModel() {
-    fun getSession():LiveData<UserModel>{
+class HomeViewModel(private val repository: RecommendationRepository) : ViewModel() {
+    fun getSession(): LiveData<UserModel> {
         return repository.getSession().asLiveData()
     }
+
+    fun getAllCafeData() = repository.getAllCafeDate()
 }
