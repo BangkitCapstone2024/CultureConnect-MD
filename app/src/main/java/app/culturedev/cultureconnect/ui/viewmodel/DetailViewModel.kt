23a33d3e6d77550d4178677069c16751fd3a8acb
Gorthdzx <krislaksana123@gmail.com>
2024-12-13 10:08:45 +0700

    refactor : extend recomendation repository

--- app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/DetailViewModel.kt
@@ -2,7 +2,8 @@ package app.culturedev.cultureconnect.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import app.culturedev.cultureconnect.data.repository.CafeRepo
+import app.culturedev.cultureconnect.data.repository.RecommendationRepository
 
 class DetailViewModel(
-    private val repository: CafeRepo
+    private val repository: RecommendationRepository
 ) : ViewModel()
