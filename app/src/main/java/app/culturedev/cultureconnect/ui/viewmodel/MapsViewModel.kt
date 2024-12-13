7e54f8b5f50893e000e0607a9c6375e6022259a4
Gorthdzx <krislaksana123@gmail.com>
2024-12-13 11:49:00 +0700

    feat: call list cafe function from repo

--- app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/MapsViewModel.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/viewmodel/MapsViewModel.kt
@@ -1,9 +1,10 @@
 package app.culturedev.cultureconnect.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
-import app.culturedev.cultureconnect.data.repository.CafeRepo
+import app.culturedev.cultureconnect.data.repository.RecommendationRepository
 
-class MapsViewModel (
-    private val repository:CafeRepo
-):ViewModel() {
+class MapsViewModel(
+    private val repository: RecommendationRepository
+) : ViewModel() {
+    fun getAllCafe() = repository.getAllCafeDate()
 }
