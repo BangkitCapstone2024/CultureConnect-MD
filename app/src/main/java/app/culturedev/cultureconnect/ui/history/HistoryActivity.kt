3da052e9d76d96fa8fc5194a68f087ac517378b8
Gorthdzx <krislaksana123@gmail.com>
2024-12-31 18:01:36 +0700

    delete :  search view function and progress bar vm's

--- app/src/main/java/app/culturedev/cultureconnect/ui/history/HistoryActivity.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/history/HistoryActivity.kt
@@ -2,22 +2,11 @@ package app.culturedev.cultureconnect.ui.history
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import app.culturedev.cultureconnect.R
 import app.culturedev.cultureconnect.databinding.ActivityHistoryBinding
 import app.culturedev.cultureconnect.helper.NetworkUtil
-import app.culturedev.cultureconnect.helper.Utils
 import app.culturedev.cultureconnect.ui.MainActivity
-import app.culturedev.cultureconnect.ui.adapter.FavoriteAdapter
-import app.culturedev.cultureconnect.ui.detail.DetailActivity
-import app.culturedev.cultureconnect.ui.viewmodel.FavoriteViewModel
 import app.culturedev.cultureconnect.ui.viewmodel.HistoryViewModel
 import app.culturedev.cultureconnect.ui.viewmodel.factory.FactoryViewModel
 
@@ -33,37 +22,11 @@ class HistoryActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         backButton()
-        setupSearchView()
 
         if (!NetworkUtil.isOnline(this)) {
             NetworkUtil.netToast(this)
         }
 
-        val adapter = FavoriteAdapter { dataEntity ->
-            dataEntity.id.let {
-                val intent = Intent(this, DetailActivity::class.java).apply {
-                    putExtra(Utils.EXTRA_ID, it)
-                }
-                startActivity(intent)
-            }
-        }
-
-        binding.rvHistory.apply {
-            layoutManager = LinearLayoutManager(this@HistoryActivity)
-            this.adapter = adapter
-        }
-
-        historyViewModel.isLoading.observe(this) { isLoading ->
-            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
-        }
-
-        historyViewModel.listCafeHistory.observe(this) { events ->
-            if (events != null) {
-                adapter.submitList(events)
-            } else {
-                Toast.makeText(this, "No favorite cafe", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 
     private fun backButton() {
@@ -71,19 +34,5 @@ class HistoryActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
-    }
-
-    private fun setupSearchView() {
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                query?.let { historyViewModel.searchHistory(it) }
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                newText?.let { historyViewModel.searchHistory(it) }
-                return true
-            }
-        })
     }
 }
