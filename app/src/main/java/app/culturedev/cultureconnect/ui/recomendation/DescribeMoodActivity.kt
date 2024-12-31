ecc0d520add3783ca870fc050173db670b96bbd1
Gorthdzx <krislaksana123@gmail.com>
2024-12-31 18:02:19 +0700

    delete :  unused import libs

--- app/src/main/java/app/culturedev/cultureconnect/ui/recomendation/DescribeMoodActivity.kt
+++ app/src/main/java/app/culturedev/cultureconnect/ui/recomendation/DescribeMoodActivity.kt
@@ -4,19 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import app.culturedev.cultureconnect.R
 import app.culturedev.cultureconnect.data.result.ResultCafe
 import app.culturedev.cultureconnect.databinding.ActivityDescribeMoodBinding
 import app.culturedev.cultureconnect.helper.NetworkUtil
 import app.culturedev.cultureconnect.helper.Utils
 import app.culturedev.cultureconnect.ui.viewmodel.DescribeMoodViewModel
 import app.culturedev.cultureconnect.ui.viewmodel.factory.RecommendationFactoryViewModel
-import java.util.ArrayList
 
 class DescribeMoodActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDescribeMoodBinding
@@ -58,7 +53,8 @@ class DescribeMoodActivity : AppCompatActivity() {
                             Intent(this@DescribeMoodActivity, MoodResultActivity::class.java)
                         intent.putExtra(Utils.PREDICT_MOOD, result.data.predictedMood)
                         intent.putExtra(Utils.MOOD_QUOTE, result.data.quote)
-                        val arrayListRecomendation = result.data.cafeRecommendation?.filterNotNull()?.let { ArrayList(it) }
+                        val arrayListRecomendation =
+                            result.data.cafeRecommendation?.filterNotNull()?.let { ArrayList(it) }
                         intent.putParcelableArrayListExtra(Utils.LIST_PLACE, arrayListRecomendation)
                         startActivity(intent)
                         finish()
